d4c90d9e6faaba6bb558a40d30549bd2ade92465
Matheus <matheusalexandrecm@gmail.com>
2020-05-04 14:12:21 -0300

    04-05-2020

---
+++ app/src/main/java/com/example/atmconsultoria/DetalhesEmpresaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.atmconsultoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesEmpresaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_empresa)
+    }
+}
